8c628faefcf0106dd51b137ad3207d9525d9f6b5
kushthakker <kushthakker777@gmail.com>
2021-07-23 18:46:28 +0530

    production

--- src/api/Yt.js
+++ src/api/Yt.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAsRjPfiG75tEnJytM3s36609CpxpTbWHg";
+const KEY = process.env.REACT_APP_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
